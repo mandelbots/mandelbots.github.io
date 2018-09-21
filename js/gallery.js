60308f5a43acf165708f116f6388880292fa328a
Thanh-Phuong <python0429@gmail.com>
2018-09-21 16:16:59 -0400

    Add footer, sponsor references

--- js/gallery.js
+++ js/gallery.js
@@ -2,20 +2,15 @@
  * @author Landmaster
  */
 
-const GALLERY_LEN = document.getElementsByClassName('galleryImg').length;
+const GALLERY_LEN = document.getElementsByClassName('galleryCont').length;
 
 let curImgIdx = 0;
 
 function updateGallery() {
-	let images = document.getElementsByClassName('galleryImg');
-	let prevActive = document.querySelector('.galleryImg.active');
+	let conts = document.getElementsByClassName('galleryCont');
+	let prevActive = document.querySelector('.galleryCont.active');
 	if (prevActive) prevActive.classList.remove('active');
-	images[curImgIdx].classList.add('active');
-	
-	let galleryDescs = document.getElementsByClassName('galleryDesc');
-	prevActive = document.querySelector('.galleryDesc.active');
-	if (prevActive) prevActive.classList.remove('active');
-	galleryDescs[curImgIdx].classList.add('active');
+	conts[curImgIdx].classList.add('active');
 	
 	setTimeout(function () { ++curImgIdx; curImgIdx %= GALLERY_LEN; updateGallery(); }, 6000);
 }

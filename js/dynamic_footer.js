75f56e04acbcbdfc9d004c13cc36f6987134629a
Thanh-Phuong <python0429@gmail.com>
2018-09-22 21:21:59 -0400

    Add dynamic footer resizing

---
+++ js/dynamic_footer.js
@@ -0,0 +1,7 @@
+/**
+ * @author Landmaster
+ */
+
+window.addEventListener("load", function () {
+	document.body.style.paddingBottom = document.getElementsByTagName('footer')[0].clientHeight + 'px';
+});
